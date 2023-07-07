ae7c5bdb2f26d916e6ac75a93c1d60d207dbe579
dedsergiio <124468587+dedsergiio@users.noreply.github.com>
2023-07-07 01:57:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function mostrar(){
+    document.getElementById('inicio').style.display = 'none';
+    document.getElementById('contenedor').style.display = 'none';
+    document.getElementById('mensaje').style.display = 'block';
+}
+
